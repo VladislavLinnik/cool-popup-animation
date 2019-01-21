f724c0dc8953e05f4459c245c72254fd4902ad9b
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-01-21 17:26:26 +0200

    Made the menu and its animation

---
+++ app/js/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+$('button').on('click', function(){
+    $('body').toggleClass('open');
+});
+
+// Smooth transition by anchors
+$("#smooth").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор блока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 500 мс
+    $('body,html').animate({scrollTop: top}, 500);
+});
